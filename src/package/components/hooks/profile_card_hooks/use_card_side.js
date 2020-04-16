6837df864fbc605bbf433608aeedd88cf533196b
Tom Gilbertson <tom@appetizeapp.com>
2020-04-15 20:18:00 -0700

    First Commit

---
+++ src/package/components/hooks/profile_card_hooks/use_card_side.js
@@ -0,0 +1,26 @@
+import { useCallback, useContext } from 'react';
+
+import { ProfileCardContext } from '../../commons/profile_card/profile_card';
+
+import { SET_SIDE } from '../../../store/profile_card/profile_card_actions_types';
+
+export const useCardSide = () => {
+    const { state, dispatch } = useContext(ProfileCardContext);
+    const { side, changingSides } = state;
+
+    const setCardSide = useCallback(
+        (newSide) => {
+            console.log('click ', side, newSide, changingSides);
+            if (changingSides) {
+                return;
+            }
+            dispatch({
+                type: SET_SIDE,
+                side: newSide
+            });
+        },
+        [changingSides]
+    );
+
+    return [side, setCardSide];
+};
