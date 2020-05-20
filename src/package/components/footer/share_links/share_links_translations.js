f3d81341b7d10fa16fd6005e6ca62488e6a997dd
Tom Gilbertson <tomgilbertson4@gmail.com>
2020-05-19 19:30:52 -0700

    Footer

--- src/package/components/footer/share_links/share_links_translations.js
+++ src/package/components/footer/share_links/share_links_translations.js
@@ -3,6 +3,6 @@ import { defineMessages } from 'react-intl';
 export const translations = defineMessages({
     linkMessage: {
         id: 'Footer.link.message',
-        defaultMessage: 'Get to know me more, discover my profile on @welovedevs ➡️ {link}'
+        defaultMessage: 'Get to know me more, discover my profile at {link}'
     }
 });
